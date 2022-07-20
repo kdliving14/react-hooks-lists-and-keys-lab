f83d716a5bdf5a4dc173981493c03b9f490c3ba0
Karter <kdlmuzixnerd14@gmail.com>
2022-07-20 09:26:31 -0700

    commit tree c6756261110555b6013b7913c80f1771438eac6c

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,7 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map(tech => <span key={name+tech}>{tech}</span>)}
       </div>
     </div>
   );
